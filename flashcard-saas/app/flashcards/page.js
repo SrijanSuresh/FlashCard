bd9a4743a34a2e15fcfa3e39a7bd8c367091a93f
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-08-16 09:14:43 -0500

    fixed the everlating bug of cards not displaying

--- flashcard-saas/app/flashcards/page.js
+++ flashcard-saas/app/flashcards/page.js
@@ -14,45 +14,41 @@ import {
     CardContent
   } from '@mui/material'
 
-export default function Flashcards() {
-
-    const {isLoaded, isSignedIn, user} = useUser()
-    const [flashcards, setFlashcards] = useState([])
-    const router = useRouter()
-
+  export default function Flashcards() {
+    const { isLoaded, isSignedIn, user } = useUser();
+    const [flashcards, setFlashcards] = useState([]);
+    const router = useRouter();
+  
     useEffect(() => {
-        async function getFlashcards() {
-            if (!user) return
-            const docRef = doc(collection(db, 'users'), user.id)
-            const docSnap = await getDoc(docRef)
-
-            if(docSnap.exists()) {
-                const collection = docSnap.data().flashcards || []
-                setFlashcards(collection)
-            }else{
-                await setDoc(docRef, {flashcards: []})
-            }
+      async function getFlashcards() {
+        if (!user) return;
+        const docRef = doc(collection(db, 'users'), user.id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          const collections = docSnap.data().flashcards || [];
+          setFlashcards(collections);
+        } else {
+          await setDoc(docRef, { flashcards: [] });
         }
-
-        if (user) {
-            getFlashcards()
-        }
-    }, [user])
-
-    if (!isLoaded || !isSignedIn){
-        return <></>
+      }
+      getFlashcards();
+    }, [user]);
+  
+    if (!isLoaded || !isSignedIn) {
+      return <></>;
     }
-console.log(flashcards.length)
-const handleCardClick = (id) => {
-  router.push(`/flashcard?id=${id}`)
-}
+  
+    const handleCardClick = (id) => {
+      router.push(`/flashcard?id=${id}`);
+    };
+  
     return (
         <Container maxWidth="lg">
             <Grid container spacing={3} sx={{mt:4}}>
                 {flashcards.map((flashcard, index) => (
                     <Grid item xs={12} sm={6} md={4} key={index}>
                         <Card>
-                            <CardActionArea onClick={() => handleCardClick(index)}>
+                            <CardActionArea onClick={() => handleCardClick(flashcard.name)}>
                                 <CardContent>
                                     <Typography variant="h6" >
                                         {flashcard.name}
