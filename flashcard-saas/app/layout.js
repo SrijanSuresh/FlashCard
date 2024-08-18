56eec57d77447241406cb73b9ae91690a9937d4d
SrijanSuresh <126934224+SrijanSuresh@users.noreply.github.com>
2024-08-18 11:57:10 -0500

    enabled analytics

--- flashcard-saas/app/layout.js
+++ flashcard-saas/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
         
         <body className={inter.className}>{children}</body>
       </html>
+      <Analytics/>
     </ClerkProvider>
   );
 }
